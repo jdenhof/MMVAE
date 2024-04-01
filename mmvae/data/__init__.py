c305f83c8d738b162b30b19122efc07ebad89e8d
Jagger D. Denhof <denhofja@mail.gvsu.edu>
2024-04-01 15:54:04 -0400

    removed cellxgene import

--- mmvae/data/__init__.py
+++ mmvae/data/__init__.py
@@ -1,9 +1,8 @@
-from mmvae.data.loaders import ChunkedCellCensusDataLoader, MultiModalLoader, configure_singlechunk_dataloaders, configure_multichunk_dataloaders, create_cell_census_dataloaders
+from mmvae.data.loaders import ChunkedCellCensusDataLoader, MultiModalLoader, configure_singlechunk_dataloaders, configure_multichunk_dataloaders
 
 __all__ = [
     'configure_singlechunk_dataloaders',
     'configure_multichunk_dataloaders',
-    'create_cell_census_dataloaders'
     "ChunkedCellCensusDataLoader",
     "MultiModalLoader",
 ]
